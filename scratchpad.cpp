7feedac48251d7333a4b3ada9ccce4d5ca4d163f
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2021-09-10 19:37:07 -0400

    Update the scratchpad

--- scratchpad.cpp
+++ scratchpad.cpp
@@ -1,6 +1,10 @@
+#include <array>
+#include <chrono>
 #include <iostream>
 #include <memory>
+#include <tuple>
 #include <vector>
+#include <sstream>
 
 struct A {
   virtual ~A() = default;
@@ -9,16 +13,38 @@ struct A {
 struct B : A {
 };
 
-int main() {
-  std::shared_ptr<A> a(new A());
-  std::shared_ptr<B> b = std::dynamic_pointer_cast<B>(a);
 
-  if (b) {
-    std::cout << "Downcast is successful";
-  } else {
-    std::cout << "It is not";
+int ExtractFirstIntFromString(const std::string& str) {
+  static const char* const digits = "0123456789";
+  const std::size_t first_digit_pos = str.find_first_of(digits);
+  if (first_digit_pos != std::string::npos) {
+    const std::size_t first_non_digit_pos_after_digit = str.find_first_not_of(digits, first_digit_pos);
+    return std::stoi(
+             str.substr(first_digit_pos,
+                        first_non_digit_pos_after_digit != std::string::npos ?
+                          first_non_digit_pos_after_digit - first_digit_pos :
+                          first_non_digit_pos_after_digit)
+                    );
   }
-  std::cout << std::endl;
+  return -1;
+}
+
+static inline int ExtractLastIntFromString(const std::string& str) {
+  static const char* const digits = "0123456789";
+  size_t last_index = str.find_last_not_of(digits);
+  return std::stoi(str.substr(last_index + 1));
+}
+
+
+int main() {
+  int ints = ExtractFirstIntFromString("123456_12354566_swkjrnwierjn_234");
+  std::string func_name = "tvmgen_default_fused_nn_batch_matmul_1_0_kernel0";
+  func_name = func_name.substr(0, func_name.find_last_of("_kernel") - 6);
+
+  std::cout << ExtractLastIntFromString(func_name) << std::endl;
+
+  std::cout << std::numeric_limits<int>().min() << " : "
+            << std::numeric_limits<int>().max();
 
   return 0;
 }
