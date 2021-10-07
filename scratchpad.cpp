6aa92dbce7d460397cf1ded6656cf630c1e6f540
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2021-10-07 18:42:03 -0400

    Update the scratchpads

--- scratchpad.cpp
+++ scratchpad.cpp
@@ -6,45 +6,12 @@
 #include <vector>
 #include <sstream>
 
-struct A {
-  virtual ~A() = default;
-};
-
-struct B : A {
-};
-
-
-int ExtractFirstIntFromString(const std::string& str) {
-  static const char* const digits = "0123456789";
-  const std::size_t first_digit_pos = str.find_first_of(digits);
-  if (first_digit_pos != std::string::npos) {
-    const std::size_t first_non_digit_pos_after_digit = str.find_first_not_of(digits, first_digit_pos);
-    return std::stoi(
-             str.substr(first_digit_pos,
-                        first_non_digit_pos_after_digit != std::string::npos ?
-                          first_non_digit_pos_after_digit - first_digit_pos :
-                          first_non_digit_pos_after_digit)
-                    );
-  }
-  return -1;
-}
-
-static inline int ExtractLastIntFromString(const std::string& str) {
-  static const char* const digits = "0123456789";
-  size_t last_index = str.find_last_not_of(digits);
-  return std::stoi(str.substr(last_index + 1));
-}
-
-
 int main() {
-  int ints = ExtractFirstIntFromString("123456_12354566_swkjrnwierjn_234");
-  std::string func_name = "tvmgen_default_fused_nn_batch_matmul_1_0_kernel0";
-  func_name = func_name.substr(0, func_name.find_last_of("_kernel") - 6);
-
-  std::cout << ExtractLastIntFromString(func_name) << std::endl;
-
-  std::cout << std::numeric_limits<int>().min() << " : "
-            << std::numeric_limits<int>().max();
-
+  std::cout << "[\n\
+  [1, 1, 1, 1, ]\n\
+  [1, 4, 1, 32, ]\n\
+  [2, 64, 1, 1, ]\n\
+  [1, 16, ]\n\
+]";
   return 0;
 }
--- scratchpad.py
+++ scratchpad.py
@@ -1,16 +1,5 @@
-def A(a, b, c):
-    print(a, b, c)
+def test_config(pytestconfig):
+    pass
 
-
-def B():
-    return 2, 3
-
-
-A(*B(), 3)
-
-A = [1, 2, 3]
-
-for a in A:
-    if a == 3:
-        A.append(4)
-    print(a)
+def test_no_config():
+    pass
