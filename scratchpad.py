71c4be3b2299661bfacbc5635c4c37e4a8db6638
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2021-10-26 19:49:08 -0400

    Update the scratchpad

--- scratchpad.py
+++ scratchpad.py
@@ -1,5 +1,11 @@
-def test_config(pytestconfig):
-    pass
+class A:
 
-def test_no_config():
-    pass
+    @property
+    def a(self):
+        return 1
+
+o = A()
+print([attr if isinstance(attr, property) else None for attr in dir(o)])
+
+d = {k: getattr(o, k, '') for k in o.__dir__() if k[:2] != '__' and type(getattr(o, k, '')).__name__ != 'method'}
+print(d)
