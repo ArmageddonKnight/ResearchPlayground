9a34aeff70f15161c8497b5fc81c9000ace521ce
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-04-09 14:49:21 -0400

    Update the scratchpad and the gitignore rule

--- scratchpad.cpp
+++ scratchpad.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tuple>
 #include <vector>
 
 template<typename T>
@@ -13,8 +14,24 @@ inline std::vector<size_t> UnpackVectorShape(const std::vector<std::vector<T>>& 
   return subvec_shape;
 }
 
+template<typename T>
+struct VectorStripper {
+  typedef T type;
+};
+
+template<typename T>
+struct VectorStripper<std::vector<T>> {
+  typedef typename VectorStripper<T>::type type;
+};
+
+std::tuple<int, int, int> f() {
+  return {3, 4, 5.0};
+}
+
 int main() {
   std::vector<std::vector<std::vector<int>>> A(3, std::vector<std::vector<int>>(4, std::vector<int>(5, 0)));
+
+  typename VectorStripper<std::vector<std::vector<int>>>::type a = 3;
 
   std::vector<size_t> shape = UnpackVectorShape(A);
   std::cout << shape.size() << std::endl;
@@ -23,6 +40,10 @@ int main() {
     std::cout << i << ", ";
   }
   std::cout << "]" << std::endl;
+
+  int x, y, z;
+  std::tie(x, y, z) = f();
+  std::cout << x << ", " << y << ", " << z;
 
   return 0;
 }
