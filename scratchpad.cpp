f57ebf868aaa9855107fa478c750d78b7701bb0e
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-06-29 08:25:08 -0700

    Update the scratchpad

--- scratchpad.cpp
+++ scratchpad.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
-#include <regex>
+#include <cassert>
+#include <type_traits>
 
 
-inline bool NameMatchesRegexPattern(const std::string& name, const std::string& pattern) {
-  return std::regex_match(name, std::regex(pattern));
-}
+template<typename C>
+struct A {
+};
+
+// template<typename TrueKing>
+// struct GetValue {
+//   static constexpr int value = 1;
+// };
+
+// template<>
+// struct GetValue<int> {
+//   static constexpr int value = 1;
+// };
+
+
+// int g();
+
+template<>
+struct [[deprecated("HappyHappy")]] A<int> {
+  // void f() {
+  //   // assert(false && "This should NOT happen");
+  //   // static_assert(GetValue<int>::value, "Happy");
+  //   // static_assert(std::is_same<int, int>::value, "Happy");
+  //   g();
+  // }
+  // int a = g(3);
+  static constexpr int a = 3;
+};
+
+template<typename C>
+struct B : public A<C> {
+  using A<C>::a;
+};
 
 int main() {
-  std::cout << NameMatchesRegexPattern("ax0_ax1_fused_123", "((ax\\d_)+)fused_(\\d+)") << std::endl;
+  struct A<float> a;
+  struct B<int> b;
+  // a.f();
+  [[deprecated]] int d;
+
+  std::cout << d << std::endl;
 
   return 0;
 }
