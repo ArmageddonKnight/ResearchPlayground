5cc4e2824f90ae382f5cbffcdad219eea2f5e7e6
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-10-16 17:12:52 -0400

    Finish recursion

---
+++ LeetCode/Q509-Fibonacci_Number.cpp
@@ -0,0 +1,31 @@
+#include "common_utilities.h"
+
+struct Solution {
+  int workspace[31];
+
+  bool initialized = false;
+
+  void Init() {
+    workspace[0] = 0;
+    workspace[1] = 1;
+
+    for (int i = 2; i < 31; ++i) {
+      workspace[i] = workspace[i - 1] + workspace[i - 2];
+    }
+  }
+
+  int fib(const int n) {
+    if (!initialized) {
+      initialized = true;
+      Init();
+    }
+    return workspace[n];
+  }
+};
+
+int main() {
+  LOG(INFO) << Solution().fib(3);
+  LOG(INFO) << Solution().fib(4);
+
+  return 0;
+}
