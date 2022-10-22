6e567a9e8efb1df4b9bdaf5175b94d3a31359333
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-10-21 20:34:37 -0400

    Update problem 240

--- LeetCode/Divide_and_Conquer/240-Search_a_2D_Matrix_II.py
+++ LeetCode/Divide_and_Conquer/240-Search_a_2D_Matrix_II.py
@@ -4,7 +4,18 @@ import numpy as np
 def searchMatrix(matrix, target):
     # The idea is to move from the TOP LEFT corner and start moving leftward and
     # downward.
-    pass
+    m, n = 1, matrix.shape[1]
+
+    while True:
+        print(m, n)
+        if matrix[m - 1, n - 1] == target:
+            return True
+        elif matrix[m - 1, n - 1] > target:
+            n = n - 1
+        elif matrix[m - 1, n - 1] < target:
+            m = m + 1
+        if n == 0 or m == (matrix.shape[0] + 1):
+            return False
 
 
 A = np.array(
@@ -15,7 +26,7 @@ A = np.array(
         [10, 13, 14, 17, 24],
         [18, 21, 23, 26, 30],
     ],
-    dtype=np.float32,
+    dtype=np.int32,
 )
 
 print(searchMatrix(A, 5))
