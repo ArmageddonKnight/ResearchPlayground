f1e48d97d35374a0186a4dfe1383bc438405142d
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-10-30 23:52:28 -0400

    Linting

--- LeetCode/BFS/127-Word_Ladder.cpp
+++ LeetCode/BFS/127-Word_Ladder.cpp
@@ -9,8 +9,9 @@ public:
   int ladderLength(const std::string &beginWord, const std::string &endWord,
                    const std::vector<std::string> &wordList) {
 
-    // `dict` keeps track of which node has been visited before.
-    std::unordered_set<std::string> dict(wordList.begin(), wordList.end());
+    // `unvisited_words` keeps track of which node has NOT been visited before.
+    std::unordered_set<std::string> unvisited_words(wordList.begin(),
+                                                    wordList.end());
     std::queue<std::string> worklist;
 
     worklist.push(beginWord);
@@ -22,19 +23,19 @@ public:
 
       // Note that here in the case of BFS, we keep popping the worklist until
       // there is no workitem left.
-      for (int i = 0; i < n; i++) {
+      for (int i = 0; i < n; ++i) {
         std::string word = worklist.front();
         worklist.pop();
 
         if (word == endWord) {
           return ladder;
         }
-        dict.erase(word);
-        for (int j = 0; j < word.size(); j++) {
+        unvisited_words.erase(word);
+        for (size_t j = 0; j < word.size(); ++j) {
           char c = word[j];
-          for (int k = 0; k < 26; k++) {
+          for (int k = 0; k < 26; ++k) {
             word[j] = 'a' + k;
-            if (dict.find(word) != dict.end()) {
+            if (unvisited_words.find(word) != unvisited_words.end()) {
               worklist.push(word);
             }
           }
@@ -47,4 +48,8 @@ public:
   }
 };
 
-int main() { return 0; }
+int main() {
+  LOG(INFO) << Solution().ladderLength(
+      "hit", "cog", {"hot", "dot", "dog", "lot", "log", "cog"});
+  return 0;
+}
