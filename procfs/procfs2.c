8aa386dcd4198ba256a29b7b69951d0dcd4fe680
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-10-31 21:50:20 -0400

    Refactoring the procfs example

--- procfs/procfs2.c
+++ procfs/procfs2.c
@@ -3,22 +3,29 @@
  * https://stackoverflow.com/questions/29700466/create-proc-fs-dir-and-entry-under-a-already-existing-subdir-kernel-3-11-or-high
  */
 
-/*
 #include <asm/uaccess.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
-#include <linux/slab.h>
 
-#define C_PROC_BUFFER_MAX_SIZE 1024
-#define C_PROC_FILENAME "buffer1k"
+#define C_NV_CAPTURE_NEXT_PMA_ALLOC_IND_PROC_BUFFER_MAX_SIZE 1024
+#define C_NV_CAPTURE_NEXT_PMA_ALLOC_IND_PROC_FILENAME                          \
+  "nv_capture_next_pma_alloc_ind"
 
-static struct proc_dir_entry *MyProcFile;
-static char *MyProcBuffer = NULL;
-static unsigned long MyProcBufferSize = 0, MyProcBufferTemp = 0;
+static struct proc_dir_entry *NVCaptureNextPMAAllocIndFile;
+static char NVCaptureNextPMAAllocIndKBuffer[C_PROC_BUFFER_MAX_SIZE];
+static unsigned long NVCaptureNextPMAAllocIndKBufferSize = 0;
 
-static ssize_t _proc_file_read(struct file *file2read, char *buffer,
-                               size_t count, loff_t *offset) {
+enum NVCaptureNextPMAAllocMode_t {
+  kDefault = 0,
+  kRecord = 1,
+  kReplay = 2
+} NVCaptureNextPMAAllocMode;
+
+static ssize_t _nv_capture_next_pma_alloc_proc_file_read(struct file *file2read,
+                                                         char *buffer,
+                                                         size_t count,
+                                                         loff_t *offset) {
   printk(KERN_INFO "Proc file read (/proc/" C_PROC_FILENAME ") called\n");
 
   // if (count > MyProcBufferSize) {
@@ -41,7 +48,6 @@ static ssize_t _proc_file_read(struct file *file2read, char *buffer,
   if (count > MyProcBufferTemp) {
     count = MyProcBufferTemp;
   }
-  MyProcBufferTemp = MyProcBufferTemp - count;
   if (copy_to_user(buffer, MyProcBuffer, count)) {
     return -EFAULT;
   }
@@ -52,71 +58,60 @@ static ssize_t _proc_file_read(struct file *file2read, char *buffer,
   return count;
 }
 
-static ssize_t _proc_file_write(struct file *file2write, const char *buffer,
-                                size_t count, loff_t *offset) {
-  printk(KERN_INFO "Proc file write (/proc/" C_PROC_FILENAME ") called\n");
+static ssize_t _nv_capture_next_pma_alloc_proc_file_write(
+    struct file *file2write, const char *ubuf, size_t count, loff_t *offset) {
+  printk(KERN_INFO
+         "Proc file write (/proc/" C_NV_CAPTURE_NEXT_PMA_ALLOC_IND_PROC_FILENAME
+         ") called\n");
 
-  // MyProcBufferSize = count;
-  // if (MyProcBufferSize > C_PROC_BUFFER_MAX_SIZE) {
-  //   MyProcBufferSize = C_PROC_BUFFER_MAX_SIZE;
-  // }
-  // if (copy_from_user(MyProcBuffer, buffer, C_PROC_BUFFER_MAX_SIZE)) {
-  //   return -EFAULT;
-  // }
-
-  // printk(KERN_INFO "Received msg=%s of len=%ld from the user space\n",
-  //        MyProcBuffer, MyProcBufferSize);
-
-  // return MyProcBufferSize;
-  // return 0;
-  // MyProcBufferSize = count;
-  // if (MyProcBufferSize > C_PROC_BUFFER_MAX_SIZE) {
-  //   MyProcBufferSize = C_PROC_BUFFER_MAX_SIZE;
-  // }
-  MyProcBufferSize = count;
-  if (MyProcBufferSize > C_PROC_BUFFER_MAX_SIZE) {
-    MyProcBufferSize = C_PROC_BUFFER_MAX_SIZE;
+  NVCaptureNextPMAAllocIndKBufferSize = count;
+  if (*offset || count > C_NV_CAPTURE_NEXT_PMA_ALLOC_IND_PROC_BUFFER_MAX_SIZE) {
+    return -EFAULT
   }
-  MyProcBufferTemp = MyProcBufferSize;
-
-  if (copy_from_user(MyProcBuffer, buffer, count)) {
+  if (copy_from_user(NVCaptureNextPMAAllocIndKBuffer, ubuf, count)) {
     return -EFAULT;
   }
 
-  printk(KERN_INFO "Received msg=%s of len=%ld from the user space\n",
-         MyProcBuffer, MyProcBufferSize);
+  if (sscanf(NVCaptureNextPMAAllocIndKBuffer, "%d",
+             NVCaptureNextPMAAllocMode) != 1) {
+    return -EFAULT;
+  }
 
-  return MyProcBufferSize;
+  *offset = count;
+  return count;
 }
 
+static struct proc_ops nv_capture_next_pma_alloc_ind_proc_ops {
+  .proc_read = _nv_capture_next_pma_alloc_proc_file_read,
+  .proc_write = _nv_capture_next_pma_alloc_proc_file_write
+};
+
 int init_module() {
+  NVCaptureNextPMAAllocIndFile =
+      proc_create(C_NV_CAPTURE_NEXT_PMA_ALLOC_IND_PROC_FILENAME, 0666, NULL,
+                  nv_capture_next_pma_alloc_ind_proc_ops);
 
-  struct proc_ops my_proc_ops;
-  my_proc_ops.proc_read = _proc_file_read;
-  my_proc_ops.proc_write = _proc_file_write;
-
-  MyProcFile = proc_create(C_PROC_FILENAME, 0644, NULL, my_proc_ops);
-
-  if (MyProcFile == NULL) {
-    remove_proc_entry(C_PROC_FILENAME, NULL);
-    printk(KERN_ALERT "Error: Could not initialize /proc/" C_PROC_FILENAME
+  if (NVCaptureNextPMAAllocIndFile == NULL) {
+    proc_remove(NVCaptureNextPMAAllocIndFile);
+    printk(KERN_ALERT "Error: Could not initialize "
+                      "/proc/" C_NV_CAPTURE_NEXT_PMA_ALLOC_IND_PROC_FILENAME
                       "\n");
     return -ENOMEM;
   }
-  printk(KERN_INFO "/proc/" C_PROC_FILENAME " created\n");
-  MyProcBuffer = kmalloc(1024 * sizeof(char), GFP_KERNEL);
+  printk(KERN_INFO "/proc/" C_NV_CAPTURE_NEXT_PMA_ALLOC_IND_PROC_FILENAME
+                   " created\n");
   return 0;
 }
 
 void cleanup_module() {
-  remove_proc_entry(C_PROC_FILENAME, NULL);
-  kfree(MyProcBuffer);
-  printk(KERN_INFO "/proc/" C_PROC_FILENAME " removed\n");
+  proc_remove(NVCaptureNextPMAAllocIndFile);
+  printk(KERN_INFO "/proc/" C_NV_CAPTURE_NEXT_PMA_ALLOC_IND_PROC_FILENAME
+                   " removed\n");
 }
 
 MODULE_LICENSE("GPL");
- */
 
+/*
 #include <asm/uaccess.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -184,3 +179,4 @@ static void simple_cleanup(void) { proc_remove(ent); }
 
 module_init(simple_init);
 module_exit(simple_cleanup);
+ */
