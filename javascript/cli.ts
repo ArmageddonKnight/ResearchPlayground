845e4c2729f588eb65a1e2b116ef72d29ecc44dd
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2024-01-31 20:03:38 -0500

    Update

--- javascript/cli.ts
+++ javascript/cli.ts
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-import { validateWithSchema } from './validate';
-import { program } from 'commander';
+import { validateWithSchema } from "./validate";
+import { program } from "commander";
 
 async function main() {
   program
-    .command('schema')
-    .description('Validate YAML files against the given schema path or URL.')
-    .argument('<schema>', 'The schema to use for validation')
+    .command("schema")
+    .description("Validate YAML files against the given schema path or URL.")
+    .argument("<schema>", "The schema to use for validation")
     .argument(
-      '<patterns...>',
-      'The glob patterns for YAML files to be validated',
+      "<patterns...>",
+      "The glob patterns for YAML files to be validated",
     )
-    .option('--yamlVersion', 'The YAML version to use for validation')
+    .option("--yamlVersion", "The YAML version to use for validation")
     .action(async (schema, patterns, options) => {
       const error = await validateWithSchema(
         { yamlVersion: options.yamlVersion },
--- javascript/validate.ts
+++ javascript/validate.ts
@@ -1,23 +1,50 @@
-import { glob } from 'glob';
-import { YamlVersion } from 'yaml-language-server/out/server/src/languageservice/parser/yamlParser07';
+import { YAMLSchemaService } from "yaml-language-server/out/server/src/languageservice/services/yamlSchemaService";
+import { YAMLValidation } from "yaml-language-server/out/server/src/languageservice/services/yamlValidation";
+import { YamlVersion } from "yaml-language-server/out/server/src/languageservice/parser/yamlParser07";
+import { Diagnostic } from "vscode-languageserver-types";
 
-export interface Settings {
+export interface ValidationOptions {
   yamlVersion?: YamlVersion;
+  schema: string;
+}
+
+interface FileValidationResult {
+  filePath: string;
+  diagnostic: Diagnostic[];
+}
+
+async function _validate(
+  files: string[],
+  options: ValidationOptions,
+): Promise<FileValidationResult[]> {
+  
+}
+
+async function _validateAndOutput(files: string[], options: ValidationOptions) {
+  console.log(`Validating ${files.length} YAML files.`);
+  const validation_errors = await _validate(files, options);
+
+  if (validation_errors.length == 0) {
+    console.log("All done. No errors found.");
+    return;
+  }
+
+  console.error("Found invalid files:");
+  for (const validation_error of validation_errors) {
+    for (const error of validation_error.diagnostic) {
+      console.error(
+        `${error.filePath}:${error.range.start.line + 1}:${error.range.start.character + 1}: ${error.message}`,
+      );
+    }
+  }
+
+  return validation_errors;
 }
 
 export async function validateWithSchema(
-  settings: Settings,
-  schema: string,
-  ...patterns: string[]
+  options: ValidationOptions,
+  files: string[],
 ) {
-  console.log('Searching YAML files with patterns=', patterns);
-  const files = await Promise.all(
-    patterns.map(
-      (pattern) =>
-        new Promise<string[]>((callback, error) => {
-          glob(pattern, { nodir: true });
-        }),
-    ),
-  ).then((filesArrays) => filesArrays.flat());
-  console.log('Validating YAML files=', files);
+  console.log("Validating YAML files=", files);
+  return _validateAndOutput(files, options);
 }
