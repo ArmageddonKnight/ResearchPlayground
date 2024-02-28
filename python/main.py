b80471aada8e7d5d5af56001506ea6519aac0d18
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2024-02-28 14:02:19 -0500

    Update the examples

--- python/main.py
+++ python/main.py
@@ -1,35 +1,8 @@
-from typing import Tuple
-
-import torch
-from torch.autograd import Function
-from torch.autograd.function import FunctionCtx
+class A:
+    def __init__(self):
+        self.a: int
 
 
-class MulConstant(Function):
-    @staticmethod
-    def forward(  # type: ignore[override]
-        x: torch.Tensor, constant: int
-    ) -> torch.Tensor:
-        return x * constant
+mya = A()
 
-    @staticmethod
-    def setup_context(
-        ctx: FunctionCtx, inputs: Tuple[torch.Tensor, int], output: torch.Tensor
-    ) -> None:
-        _, constant = inputs
-        ctx.constant = constant  # type: ignore[attr-defined]
-
-    @staticmethod
-    def backward(  # type: ignore[override]
-        ctx: FunctionCtx, grad_y: torch.Tensor
-    ) -> Tuple[torch.Tensor, None]:
-        return grad_y * ctx.constant, None  # type: ignore[attr-defined]
-
-
-print(MulConstant.apply(torch.tensor(3, dtype=torch.float32), 4))
-
-x = torch.tensor(3, dtype=torch.float32)
-x.requires_grad_(True)
-y = x * 4
-y.backward()
-print(x._grad)
+print(mya)
--- python/test.py
+++ python/test.py
@@ -1,51 +1,25 @@
-import unittest
-from unittest.mock import MagicMock, call
+import unittest.mock
+from functools import partial
+
+import pytest
 
 
-# Functions to test
-def f1():
-    print("f1")
+@pytest.mark.parametrize(
+    "a", [1, 2, 3]
+)
+class TestA:
+    @pytest.fixture()
+    def fixture(self, a):
+        return a
+    
+    def test_foo(self, fixture):
+        print(fixture)
 
 
-def f2():
-    print("f2")
+mock = unittest.mock.MagicMock()
 
+mock_call = partial(mock.__call__, 3)
 
-def f3():
-    print("f3")
-
-
-# Function that invokes the functions in a specific order
-def invoke_functions():
-    f1()
-    f2()
-    f3()
-
-
-class TestFunctionOrder(unittest.TestCase):
-    def test_function_order(self):
-        # Create mock objects for the functions
-        mocked_f1 = MagicMock()
-        mocked_f2 = MagicMock()
-        mocked_f3 = MagicMock()
-
-        # Replace the original functions with the mocked ones
-        global f1, f2, f3
-        f1 = mocked_f1
-        f2 = mocked_f2
-        f3 = mocked_f3
-
-        # Invoke the function that calls the functions
-        invoke_functions()
-
-        # Assert the order of function calls
-        mocked_f1.assert_called_once()
-        mocked_f2.assert_called_once()
-        mocked_f3.assert_called_once()
-
-        print([call(), call(), call()])
-        print([call(f1()), call(f2()), call(f3())])
-
-
-if __name__ == "__main__":
-    unittest.main()
+mock(1, 2, 3)
+mock_call(4)
+print(mock.mock_calls)
